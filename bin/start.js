b078398659c91923a873dbae367cc3fa4d915ad0
iTulinov <energiay@gmail.com>
2022-09-07 16:05:59 +0300

    add module css

--- bin/start.js
+++ bin/start.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-const fs = require("fs-extra");
-const path = require("path");
-const https = require("https");
-const { exec } = require("child_process");
+const fs = require('fs-extra');
+const path = require('path');
+const https = require('https');
+const { exec } = require('child_process');
 
-const packageJson = require("../package.json");
+const packageJson = require('../package.json');
 
 const scripts = `"start": "webpack-dev-server --mode=development --open --hot",
 "build": "webpack --mode=production"`;
@@ -15,12 +15,12 @@ const getDeps = (deps) =>
     Object.entries(deps)
         .map((dep) => `${dep[0]}@${dep[1]}`)
         .toString()
-        .replace(/,/g, " ")
-        .replace(/^/g, "")
+        .replace(/,/g, ' ')
+        .replace(/^/g, '')
         // исключим зависимость, используемую только в этом файле, не относящуюся к шаблону
-        .replace(/fs-extra[^\s]+/g, "");
+        .replace(/fs-extra[^\s]+/g, '');
 
-console.log("Initializing project..");
+console.log('Initializing project..');
 
 // создадим папку и инициализируем npm-проект
 exec(
@@ -39,79 +39,81 @@ exec(
                 .toString()
                 .replace(
                     '"test": "echo \\"Error: no test specified\\" && exit 1"',
-                    scripts
+                    scripts,
                 )
                 .replace('"keywords": []', babel);
             fs.writeFile(packageJSON, data, (err2) => err2 || true);
         });
 
-        const filesToCopy = ["webpack.config.js"];
+        const filesToCopy = ['webpack.config.js'];
 
         for (let i = 0; i < filesToCopy.length; i += 1) {
             fs.createReadStream(
-                path.join(__dirname, `../${filesToCopy[i]}`)
+                path.join(__dirname, `../${filesToCopy[i]}`),
             ).pipe(
-                fs.createWriteStream(`${process.argv[2]}/${filesToCopy[i]}`)
+                fs.createWriteStream(`${process.argv[2]}/${filesToCopy[i]}`),
             );
         }
         // npm, при установке пакета, удалит файл .gitignore, поэтому его нельзя скопировать из локальной папки шаблона; этот файл нужно загрузить. После отправки кода в GitHub-репозиторий пользуйтесь raw-файлом .gitignore
         https.get(
-            "https://raw.githubusercontent.com/Nikhil-Kumaran/reactjs-boilerplate/master/.gitignore",
+            'https://raw.githubusercontent.com/Nikhil-Kumaran/reactjs-boilerplate/master/.gitignore',
             (res) => {
-                res.setEncoding("utf8");
-                let body = "";
-                res.on("data", (data) => {
+                res.setEncoding('utf8');
+                let body = '';
+                res.on('data', (data) => {
                     body += data;
                 });
-                res.on("end", () => {
+                res.on('end', () => {
                     fs.writeFile(
                         `${process.argv[2]}/.gitignore`,
                         body,
-                        { encoding: "utf-8" },
+                        { encoding: 'utf-8' },
                         (err) => {
                             if (err) throw err;
-                        }
+                        },
                     );
                 });
-            }
+            },
         );
 
-        console.log("npm init -- done\n");
-        
-        console.log("01: ", process.argv[2])
+        console.log('npm init -- done\n');
 
         // установка зависимостей
-        console.log("Installing deps -- it might take a few minutes..");
+        console.log('Installing deps -- it might take a few minutes..');
         const devDeps = getDeps(packageJson.devDependencies);
         const deps = getDeps(packageJson.dependencies);
-        console.log("02: ", devDeps)
-        console.log("03: ", deps)
-        const execStr = `cd ${process.argv[2]} && git init && node -v && npm -v && npm i -D ${devDeps}`
-        console.log(execStr)
         exec(
-            //`cd ${process.argv[2]} && git init && node -v && npm -v && npm i -D ${devDeps} && npm i -S ${deps}`,
-            execStr,
+            `cd ${process.argv[2]} && git init && node -v && npm -v && npm i -D ${devDeps} && npm i -S ${deps}`,
             (npmErr, npmStdout, npmStderr) => {
                 if (npmErr) {
-                    console.error(`Some error while installing dependencies ${npmErr}`);
+                    console.error(
+                        `Some error while installing dependencies ${npmErr}`,
+                    );
                     return;
                 }
                 console.log(npmStdout);
-                console.log("Dependencies installed");
+                console.log('Dependencies installed');
 
-                console.log("Copying additional files..");
+                console.log('Copying additional files..');
+
+                // копирование шаблонa HTML
+                fs.copy(
+                    path.join(__dirname, '../public'),
+                    `${process.argv[2]}/public`,
+                );
+
                 // копирование дополнительных файлов с кодом
                 fs.copy(
-                    path.join(__dirname, "../src"),
-                    `${process.argv[2]}/src`
+                    path.join(__dirname, '../src'),
+                    `${process.argv[2]}/src`,
                 )
                     .then(() =>
                         console.log(
-                            `All done!\n\nYour project is now ready\n\nUse the below command to run the app.\n\ncd ${process.argv[2]}\nnpm start`
-                        )
+                            `All done!\n\nYour project is now ready\n\nUse the below command to run the app.\n\ncd ${process.argv[2]}\nnpm start`,
+                        ),
                     )
                     .catch((err) => console.error(err));
-            }
+            },
         );
-    }
+    },
 );
